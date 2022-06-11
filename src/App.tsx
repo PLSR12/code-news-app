ecc300e0ff6be96368f097e89d3f809fa10f08ad
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-11 20:15:42 -0300

    added toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Routes from './routes'
 import GlobalStyle from './styles/global'
+import { ToastContainer } from 'react-toastify'
+
 
 function App() {
   return (
     <>
       <Routes />
+      <ToastContainer autoClose={2000} theme="colored" />
       <GlobalStyle />
     </>
   )
