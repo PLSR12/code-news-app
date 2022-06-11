69b550994faf9c1211df1d326e7656888e1d2e67
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-11 20:18:02 -0300

    add news routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,21 @@
 import { Switch, Route } from 'react-router-dom'
 
 import Layout from './Layout'
-import { Home } from './Pages/Home'
+import { Home, Notice } from './Pages'
+import Admin from './Pages/Admin'
+
+import paths from './config/paths'
 
 export default function RoutesPath() {
   return (
     <Switch>
+      <Route path={paths.Notices} component={Admin} />
+      <Route path={paths.NewNotice} component={Admin} />
+      <Route path={paths.EditNotice} component={Admin} />
+      <Route path={paths.NewCategory} component={Admin} />
       <Layout>
-        <Route path="/" component={Home} />
+        <Route exact path={paths.Home} component={Home} />
+        <Route exact path={"/notice/:id"} component={Notice} />
       </Layout>
     </Switch>
   )
