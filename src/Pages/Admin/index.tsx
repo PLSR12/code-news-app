0fcf6f5f1b85f17cba3c80a812e6605b02c17f5f
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-11 20:14:06 -0300

    create AdminPage

---
+++ src/Pages/Admin/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Container, ContainerItems } from './styles'
+
+import ListNotices from './ListNotices'
+import NewNotice from './NewNotice'
+import EditNotice from './EditNotice'
+import NewCategory from './NewCategory'
+
+import { SideMenuAdmin } from '../../Components'
+import paths from '../../config/paths'
+
+export default function Admin({ match: { path } }: any) {
+  return (
+    <Container>
+      <SideMenuAdmin path={path} />
+      <ContainerItems>
+        {path === paths.Notices && <ListNotices />}
+        {path === paths.NewNotice && <NewNotice />}
+        {path === paths.EditNotice && <EditNotice />}
+        {path === paths.NewCategory && <NewCategory />}
+      </ContainerItems>
+    </Container>
+  )
+}
