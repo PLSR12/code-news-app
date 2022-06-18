77ec3ecfed172efd0c701e3af0d84c8b3350d478
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-18 10:15:29 -0300

    chnaged url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiNews = axios.create({
-    baseURL: 'http://localhost:3001/'
+    baseURL: 'postgresql://postgres:mEI6mUU8255SZAJvEncF@containers-us-west-36.railway.app:6508/railway/'
 })
 
 
