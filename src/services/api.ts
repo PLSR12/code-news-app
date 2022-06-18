f0e054f42154eccadb51f3ee2405d2c83fab7cae
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-06-18 19:20:36 -0300

    chane api_url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiNews = axios.create({
-    baseURL: 'postgresql://postgres:mEI6mUU8255SZAJvEncF@containers-us-west-36.railway.app:6508/railway/'
+    baseURL: 'https://api-codenews-production.up.railway.app/'
 })
 
 
